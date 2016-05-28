133f04ca337a8f0b5deb471b78ec522d454b8ca9
IvanBohonosiuk <the.head1993@gmail.com>
2016-05-28 21:18:13 +0300

    Добавление Debugbar

--- config/app.php
+++ config/app.php
@@ -156,6 +156,9 @@ return [
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
+        // debug
+        Barryvdh\Debugbar\ServiceProvider::class,
+
     ],
 
     /*
@@ -201,6 +204,9 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+
+        // debug
+        'Debugbar' => Barryvdh\Debugbar\Facade::class,
 
     ],
 
